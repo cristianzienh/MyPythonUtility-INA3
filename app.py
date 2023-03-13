6521e4908cd13357e61cb2189e3c6423a152b092
cristianzienh <cristian.z@ienh.com.br>
2023-03-13 20:59:21 -0300

    inicio

---
+++ app.py
@@ -0,0 +1,22 @@
+import sys
+from calculadora import *
+
+#Seleciona função
+#1 - Calculadora
+#2 - Inverte string
+#3 - Ordena caracters da string
+#4 - Retorna dia da semana para data
+#5 - Detecta palíndromos
+#6 - Retorna se é número é par ou impar
+#7 - Sorteia um número aleatório para um dado intervalo numérico
+#8 - Verifica se palavra/caracter existe na string
+#9 - Imprime o número Pi com x casas decimais 
+#Enter - Sair
+print("informe a funcão:\n1 - Calculadora" +
+      "\nEnter - Sair")
+funcao = input()
+if (funcao == "1"):
+    calculate()
+else:
+    print("Função não implementada!") 
+print("Bye!")
---
+++ calculadora.py
@@ -0,0 +1,19 @@
+def calculate():
+    print("informe o cálculo a executar:")
+    args = input().split()
+    operacao = args[0]
+    n1 = int(args[1])
+    n2 = int(args[2])
+    if (operacao == "+") :
+        print("soma:", n1 + n2) 
+    elif (operacao == "-") :
+        print("subtracao:", n1 - n2)  
+def palindromo():
+    string = input("Insira a palavra ou número a ser conferida: ")
+    stringSemEspacos = string.replace(' ', '')
+    stringTodaMinuscula = stringSemEspacos.lower()
+    stringInvertida = stringTodaMinuscula[::-1]
+    if stringInvertida == stringTodaMinuscula:
+        print ("SIM")
+    else:
+        print ("NAO")
