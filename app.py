e4bfaa4a071a88ece6daeb1bc953f53d9ce11801
MATEUS BAUER <0110132@acad.redes-ienh.com.br>
2023-03-13 21:21:02 -0300

    Mateus e Felipe Palindromo

--- app.py
+++ app.py
@@ -15,8 +15,18 @@ from calculadora import *
 print("informe a funcão:\n1 - Calculadora" +
       "\nEnter - Sair")
 funcao = input()
-if (funcao == "1"):
+if (funcao == "5"):
     calculate()
 else:
     print("Função não implementada!") 
 print("Bye!")
+
+def palindromo():
+    string = input("Insira a palavra ou número a ser conferida: ")
+    stringSemEspacos = string.replace(' ', '')
+    stringTodaMinuscula = stringSemEspacos.lower()
+    stringInvertida = stringTodaMinuscula[::-1]
+    if stringInvertida == stringTodaMinuscula:
+        print ("SIM")
+    else:
+        print ("NAO")
