cd90cd0bc9c2269eb47632139a547cc467acf5d9
GabCheeseBread <127157972+GabCheeseBread@users.noreply.github.com>
2023-03-13 21:34:39 -0300

    Adicionada a função 'Data para dia de semana'.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import sys
 from calculadora import *
+from dia_semana import *
 
 #Seleciona função
 #1 - Calculadora
@@ -12,14 +13,16 @@ from calculadora import *
 #8 - Verifica se palavra/caracter existe na string
 #9 - Imprime o número Pi com x casas decimais 
 #Enter - Sair
-print("informe a funcão:\n1 - Calculadora\n9 - Pi com x casas decimais" +
+print("informe a funcão:\n1 - Calculadora\n4 - Data para dia de semana\n9 - Pi com x casas decimais" +
       "\nEnter - Sair")
 funcao = input()
-if (funcao == "5"):
+if (funcao == "1"):
     calculate()
 elif (funcao == "2"):
     string = input("Digite a string a ser invertida: ")
     print(string[::-1])
+elif (funcao == "4"):
+    dia_semana()
 elif (funcao == '9'):
     pi()
 else:
