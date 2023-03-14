7f043fdbc2b95045306ad4e5c4a4435c16c514b4
ViniciosPereira <127798812+ViniciosPereira@users.noreply.github.com>
2023-03-13 21:48:44 -0300

    arrumamos o erro do fernando

--- calculadora.py
+++ calculadora.py
@@ -12,17 +12,8 @@ def pi():
     import math
     prc = int(input('Digite o número de casas decimais --> '))
     print(f"Pi = {math.pi:.{prc}f}")
-
-<<<<<<< HEAD
-=======
-def par_ou_impar(numero):
-    if numero % 2 == 0:
-        return "par"
-    else:
-        return "ímpar"
 def ordenar_string():
     lista_caracteres = list(input("digite alguma palavra"))
     lista_caracteres.sort()
     nova_string = "".join(lista_caracteres)
     print(nova_string)
->>>>>>> 9fe0d3c40b4e62e338baa04f11956b4c8b8cf2f2
