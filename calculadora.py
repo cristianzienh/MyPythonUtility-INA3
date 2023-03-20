d7ac95ab395bc2e89eeaad530cc6603a1a09b194
EnzoLN <enzolneumann@gmail.com>
2023-03-20 19:21:45 -0300

    nao fiz nada

--- calculadora.py
+++ calculadora.py
@@ -1,8 +1,8 @@
 def calculate():
     print("informe o cálculo a executar:")
     args = input().split()
-    operacao = args[0]
-    n1 = int(args[1])
+    operacao = args[1]
+    n1 = int(args[0])
     n2 = int(args[2])
     if (operacao == "+") :
         print("soma:", n1 + n2) 
