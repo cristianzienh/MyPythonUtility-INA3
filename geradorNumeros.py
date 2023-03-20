25bde3b05381c755b5231226dddda7f7a11ff4c7
l-eduardo-l <119344990+l-eduardo-l@users.noreply.github.com>
2023-03-20 20:29:11 -0300

    att 5

--- geradorNumeros.py
+++ geradorNumeros.py
@@ -2,6 +2,7 @@ import random
 
 def random_number(start, end):
     return random.randint(start, end)
-
-print(random_number(1, 100)) # Gera um número aleatório entre 1 e 100
-print(random_number(-50, 50)) # Gera um número aleatório entre -50 e 50
+num1 = float(input("escrevca um número para iniciar: "))
+num2 = float(input("escreva um número para fechar"))
+print(random_number(num1,num2)) # Gera um número aleatório entre 1 e 100
+#print(random_number(-50, 50)) # Gera um número aleatório entre -50 e 50
