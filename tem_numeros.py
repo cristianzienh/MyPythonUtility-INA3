7e7727d09fe31708ad6720ba35d3e3b4c3680527
irruan123 <127157837+irruan123@users.noreply.github.com>
2023-03-20 21:02:30 -0300

    separação de função

--- tem_numeros.py
+++ tem_numeros.py
@@ -1,4 +1,7 @@
 def tem_numeros():
+    vari=input("digite uma palavra e/ou número")
+    print(ver_num(vari))
+def ver_num(vari):
     string = input("Digite a string a ser verificada: ")
     if any(char.isdigit() for char in string):
         print("A string contém números.")
