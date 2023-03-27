2cc1cdda381635d435a081fc96e78a5f34767fec
irruan123 <127157837+irruan123@users.noreply.github.com>
2023-03-27 19:50:48 -0300

    test tem numeros

--- tem_numeros.py
+++ tem_numeros.py
@@ -2,8 +2,7 @@ def tem_numeros():
     vari=input("digite uma palavra e/ou número")
     print(ver_num(vari))
 def ver_num(vari):
-    string = input("Digite a string a ser verificada: ")
-    if any(char.isdigit() for char in string):
-        print("A string contém números.")
+    if any(char.isdigit() for char in vari):
+        return("true")
     else:
-        print("A string não contém números.")
+        return("false")
---
+++ teste_tem_numeros.py
@@ -0,0 +1,4 @@
+from tem_numeros import *
+
+def test_ver_num():
+    assert ver_num("dcba1") =="true"
