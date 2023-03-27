0acf51c7e7660b9f616aec20a516bb0664f747c7
l-eduardo-l <119344990+l-eduardo-l@users.noreply.github.com>
2023-03-27 20:37:59 -0300

    !!alteração!!

--- geradorNumeros.py
+++ geradorNumeros.py
@@ -3,7 +3,7 @@ import random
 def random_number():
     num1 = float(input("escrevca um número para iniciar: "))
     num2 = float(input("escreva um número para fechar: "))
-    print (random.randint(num1, num2))
+    print (gerador(num1, num2))
 
-def test(num1,num2):
-    return num1 < num2 or num1 > num2 
+def gerador(num1, num2):
+    return random.randint(num1, num2)
--- test_geradorNumero.py
+++ test_geradorNumero.py
@@ -1,4 +1,6 @@
-from geradorNumeros import*
+from geradorNumeros import *
 
 def test_gerador():
-    assert test(num1,num2) num1 < num2 or num1 > num2 
+    resultado = gerador(1, 10)
+    assert resultado <= 10
+    assert resultado >= 1
